1dcf60a956e7577ec0e4bc793450a5e648f0900a
Žiga Kralj <me@zigakralj.com>
2025-12-28 11:08:17 +0100

    Dodana dokerizacija

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM php:8.4-apache
+
+COPY . /var/www/html/
+RUN chown -R www-data:www-data /var/www/html/
+
+EXPOSE 80
--- baza.php
+++ baza.php
@@ -5,8 +5,35 @@ $password = "geslo";
 $dbname = "veselica";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
+}
+
+// prilagoditve za dokerizacijo - če baza ne obstaja, jo ustvari in naloži začetne podatke
+$obstaja = $conn->query("SHOW DATABASES LIKE '$dbname';")->num_rows;
+
+if (!$obstaja) {
+	$sql = "CREATE DATABASE $dbname";
+	$conn->query($sql);
+	$conn->select_db($dbname);
+
+	$filename = 'veselica-1.sql';
+
+	$templine = '';
+	$lines = file($filename);
+	foreach ($lines as $line) {
+		if (substr($line, 0, 2) == '--' || $line == '')
+			continue;
+
+		$templine .= $line;
+
+		if (substr(trim($line), -1, 1) == ';') {
+			$conn->query($templine);
+			$templine = '';
+		}
+	}
+} else {
+	$conn->select_db($dbname);
 }
